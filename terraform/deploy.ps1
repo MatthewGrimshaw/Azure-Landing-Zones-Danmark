a451f472425eb7a21fc3cd1c8951842fb2065de7
MatthewGrimshaw <Matthew.Grimshaw@Microsoft.com>
2022-12-30 18:06:44 +0100

    IMPORT terraform

--- terraform/deploy.ps1
+++ terraform/deploy.ps1
@@ -14,3 +14,28 @@ terraform plan -out assignmnet.tfplan
 terraform apply assignmnet.tfplan
 
 Pop-Location
+
+
+### Onboarding
+
+# get the terraform
+
+$resourcesToImport = Get-Content .\modules\management_groups\canary\import_resources.json | ConvertFrom-Json 
+
+foreach($resource in $resourcesToImport.properties.managementGroups){
+    write-output $resource.name
+    write-output $resource.value
+    write-output " "
+    write-output (Get-AzManagementGroup -GroupName $resource.value).Id
+}
+
+
+Get-Member -MemberType NoteProperty | ForEach-Object {
+    write-output $_.Name
+    write-output $_.
+    write-output " "
+    terraform import terraform_id azure_resource_id
+}
+
+
+
