8946f550213694cdcf11932f9afc67573ce1e05d
MatthewGrimshaw <Matthew.Grimshaw@Microsoft.com>
2022-12-31 06:09:04 +0100

    terra

--- terraform/scripts/import-resources.ps1
+++ terraform/scripts/import-resources.ps1
@@ -15,10 +15,30 @@
 param (
     [String]
     $importFile, #= ".\modules\management_groups\canary\import_resources.json",
-    $resourceType #= "azurerm_management_group"
+    $resourceType, #= "azurerm_management_group"
+    $storageAccountName, #= "mgmtstorageqwerty",
+    $ResourceGroupName, #= "Management",
+    $containerName #= "tfstate-canary"
 )
 
 write-output $importFile
+write-output $resourceType
+write-output $storageAccountName
+write-output $ResourceGroupName
+write-output $containerName
+
+# Find State File and Generate SAS Key
+$Context = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $storageAccountName).Context
+$blobName = Get-AzStorageBlob -Container $containerName -Context $Context | Select-Object -Property Name
+$FullUri = New-AzStorageBlobSASToken -Context $Context `
+    -Container $containerName `
+    -Blob $blobName `
+    -Permission racwd `
+    -ExpiryTime (Get-Date).AddMinutes(30) `
+    -FullUri
+
+write-output $FullUri
+
 $resourcesToImport = Get-Content $importFile  | ConvertFrom-Json 
 
 
@@ -26,7 +46,7 @@ foreach($resource in $resourcesToImport.properties.managementGroups){
     $resourceId = (Get-AzManagementGroup -GroupId $resource.name).Id
     write-output "Resources to be imported are:"
     write-output "$($resourceType).$($resource.name) $resourceId"
-    terraform import "$($resourceType).$($resource.name)" $resourceId
+    terraform import -config=$FullUri "$($resourceType).$($resource.name)" $resourceId 
 }
 
 
