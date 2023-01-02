fd6d6ff3bdc5393ee1d4ecefc28fa924f25fe3c8
MatthewGrimshaw <Matthew.Grimshaw@Microsoft.com>
2023-01-02 08:18:15 +0100

    import

--- terraform/scripts/import-resources.ps1
+++ terraform/scripts/import-resources.ps1
@@ -14,12 +14,12 @@
 
 param (
     [String]
-    $importFile, #= ".\modules\management_groups\canary\import_resources.json",
-    $resourceType, #= "azurerm_management_group"
-    $storageAccountName, #= "mgmtstorageqwerty",
-    $ResourceGroupName, #= "Management",
-    $containerName, #= "tfstate-canary"
-    $tfStateFile
+    $importFile, # = ".\modules\management_groups\canary\import_resources.json"'
+    $resourceType, # = "azurerm_management_group"
+    $storageAccountName, # = "mgmtstorageqwerty"
+    $ResourceGroupName, # = "Management"
+    $containerName, # = "tfstate-canary"
+    $tfStateFile # = "terraform.tfstate"
 )
 
 write-output $importFile
@@ -27,18 +27,7 @@ write-output $resourceType
 write-output $storageAccountName
 write-output $ResourceGroupName
 write-output $containerName
-
-# Find State File and Generate SAS Key
-$Context = (Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $storageAccountName).Context
-#$blobName = Get-AzStorageBlob -Container $containerName -Context $Context | Select-Object -Property Name
-$FullUri = New-AzStorageBlobSASToken -Context $Context `
-    -Container $containerName `
-    -Blob $tfStateFile `
-    -Permission racwd `
-    -ExpiryTime (Get-Date).AddMinutes(30) `
-    -FullUri
-
-write-output $FullUri
+write-output $tfStateFile
 
 $resourcesToImport = Get-Content $importFile  | ConvertFrom-Json 
 
@@ -47,8 +36,8 @@ terraform init -backend-config storage_account_name=$storageAccountName -backend
 foreach($resource in $resourcesToImport.properties.managementGroups){
     $resourceId = (Get-AzManagementGroup -GroupId $resource.name).Id
     write-output "Resources to be imported are:"
-    write-output "$($resourceType).$($resource.name) $resourceId"
-    terraform import "$($resourceType).$($resource.name)" $resourceId 
+    write-output "$($resourceType).$(($resource.name).Replace("-", "_")) $resourceId"
+    terraform import "$($resourceType).$(($resource.name).Replace("-", "_"))" $resourceId 
 }
 
 
