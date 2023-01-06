f6409f6b8115371bbb09a1a14a30c49446665b30
MatthewGrimshaw <Matthew.Grimshaw@Microsoft.com>
2023-01-06 10:55:29 +0100

    mgmt

--- terraform/modules/management_resources/output.tf
+++ terraform/modules/management_resources/output.tf
@@ -12,3 +12,23 @@ output "id" {
   description = ""
   value       = azurerm_resource_group.Management.id
 }
+
+output "automation_account_name" {
+  description = ""
+  value       = azurerm_automation_account.automation_account.name
+}
+
+output "aurerm_network_ddos_protection_plan" {
+  description = ""
+  value       = azurerm_network_ddos_protection_plan.ddos.name
+}
+
+output "aurerm_network_ddos_protection_plan" {
+  description = ""
+  value       = azurerm_network_ddos_protection_plan.ddos.name
+}
+
+output "azurerm_storage_account" {
+  description = ""
+  value       = azurerm_storage_account.storage.name
+}
