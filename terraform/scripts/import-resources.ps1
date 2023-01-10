12dc1650b76e2abf9178e304c01f9f6289449af3
MatthewGrimshaw <Matthew.Grimshaw@Microsoft.com>
2023-01-10 11:22:59 +0100

    import

--- terraform/scripts/import-resources.ps1
+++ terraform/scripts/import-resources.ps1
@@ -117,7 +117,7 @@ foreach($resource in $resourcesToImport.properties.resource){
                 write-output $resource.scope
                 write-output $resource.name
                 $resourceId = (Get-AzRoleAssignment -Scope $resource.scope | Where-Object {($_.RoleDefinitionId -eq "8e3af657-a8ff-443c-a75c-2fe8c4bcb635") -and ($_.DisplayName -eq $resource.name)}).RoleAssignmentId
-                Get-AzRoleAssignment -Scope $resource.scope | Where-Object {($_.RoleDefinitionId -eq "8e3af657-a8ff-443c-a75c-2fe8c4bcb635") -and ($_.DisplayName -eq $resource.name)}).RoleAssignmentId
+                Get-AzRoleAssignment -Scope $resource.scope | Where-Object {($_.RoleDefinitionId -eq "8e3af657-a8ff-443c-a75c-2fe8c4bcb635") -and ($_.DisplayName -eq $resource.name)}
                 write-output "ResourceId"
                 write-output $resourceId
             }
