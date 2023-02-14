9a795ff27e404ef4502bf2bde96769256682dcf0
MatthewGrimshaw <Matthew.Grimshaw@Microsoft.com>
2023-02-14 20:47:22 +0100

    synhc me

--- terraform/modules/policy_initiatives/canary/variables.tf
+++ terraform/modules/policy_initiatives/canary/variables.tf
@@ -11,8 +11,8 @@ variable prefix {
 }
 
 locals {
-  json_files = fileset("../../../modules/policies/initiatives", "*.json")
-  json_data  = [ for f in local.json_files : jsondecode(file("../../../modules/policies/initiatives/${f}"))]
+  json_files = fileset("../../../../modules/policies/initiatives", "*.json")
+  json_data  = [ for f in local.json_files : jsondecode(file("../../../../modules/policies/initiatives/${f}"))]
   json_map = [
     for items in local.json_data: {
       category = items.properties.metadata.category
